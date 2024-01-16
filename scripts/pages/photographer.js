e484c61f9ae703b2f4ced52e24c3d2c928c112d5
Vanessa-Peixoto <vanessa.peixoto.dev@gmail.com>
2024-01-16 13:41:56 +0100

    show pictures in photographe page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,17 +1,16 @@
 async function getDetailsPhotographers() {
     const response = await fetch("../../data/photographers.json");
-    const media = await response.json();
-
-    return media;
+    const photographerData = await response.json();
+    return photographerData;
+    
 }
 
-async function displayData(media) {
+
+async function displayData(photographerData) {
     //recupère id dans l'url
     //compare id avec mon tableau media
     //affiche les medias en question 
-}
-
-// Obtenez l'URL actuelle
+    // Obtenez l'URL actuelle
 const url = new URL(window.location.href);
 
 // Récupérez les paramètres de l'URL
@@ -22,9 +21,37 @@ const id = params.get('id');
 
 console.log(id);
 
+const mediaFiltre = photographerData.media.filter(data => data.photographerId === parseInt(id));
+const photographerFiltre = photographerData.photographers.filter(data => data.id === parseInt(id))[0];
+
+console.log(mediaFiltre);
+
+const name = photographerFiltre.name.split(' ')[0].replace('-', ' ')
+const mediaSection = document.querySelector('.section-media');
+
+
+for(let i = 0; i<mediaFiltre.length; i++) {
+    const img = mediaFiltre[i].image;
+    const path = 'assets/images/' + name + '/' + img;
+    const imgElement = document.createElement('img');
+    imgElement.setAttribute('src', path);
+    imgElement.alt = mediaFiltre[i].title;
+    mediaSection.appendChild(imgElement);
+
+}
+
+
+}
 
 
 
 
+
+async function main() {
+    const photographerData = await getDetailsPhotographers();
+    await displayData(photographerData);
+}
+
+main();
 
 getDetailsPhotographers()
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -7,3 +7,123 @@ function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+
+/**
+ * @description Function which check value in name field
+ * @param {*} element 
+ * @returns boolean
+ */
+function checkNameValue(element) {
+    //condition if element is not empty and >2
+    const nameRegex = new RegExp('^[a-zA-Z]+$');
+    return element.value !== '' && element.value.length >= 2 && nameRegex.test(element.value);
+}
+
+/**
+ * @description Function which check value in email field
+ * @param {*} element 
+ * @returns boolean
+ */
+function checkEmailValue(element) {
+    //new regex to verify the email
+    const emailRegex = new RegExp("[a-zA-Z.-_0-9]+@[a-zA-Z.-_0-9]+\\.[a-zA-Z.-_]+");
+    return emailRegex.test(element.value);
+}
+
+/**
+ * @description Function which check value in message field
+ * @param {*} element 
+ * @returns boolean
+ */
+function checkMessageValue(element) {
+    //condition if element is not empty and >2
+    return element.value !== '' && element.value.length >= 2;
+}
+
+
+
+/**
+ * @description Function that checks if element is valid and add show/remove an error message
+ * @param {*} element 
+ * @param {function} validationFunction 
+ * @param {string} message 
+ * @returns boolean
+ */
+function validateField(element, validationFunction, message) {
+    //the field is validated by default
+    let isVerified = true;
+
+    //if function which check element is false
+    if (!validationFunction(element)) {
+        isVerified = false
+        //show the error message
+        showErrorMessage(element, message)
+    } else {
+        //when it's ok, remove the error
+        removeError(element)
+    }
+    //get field status
+    return isVerified
+}
+
+/**
+ * @description Function which verify field with the listener change
+ */
+function verifyFields() {
+    //Select elements
+    const firstname = document.getElementById('firstname');
+    const lastname = document.getElementById('lastname');
+    const email = document.getElementById('email');
+    const message = document.getElementById('message');
+
+    //For each element, add a listener change
+    firstname.addEventListener('input', (e) => {
+        //Call function which verify the value on field and add the error message
+        validateField(e.target, checkNameValue, 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.')
+    })
+
+    lastname.addEventListener('input', (e) => {
+        validateField(e.target, checkNameValue, 'Veuillez entrer 2 caractères ou plus pour le champ du nom.')
+    })
+
+    email.addEventListener('input', (e) => {
+        validateField(e.target, checkEmailValue, 'Veuillez entrer un email valide.')
+    })
+
+    message.addEventListener('input', (e) => {
+        validateField(e.target, checkMessageValue, 'Vous devez écrire un message de plus de 2 caractères.')
+    })
+
+}
+
+verifyFields()
+
+/**
+ * @description Function validate form
+ * @returns boolean
+ */
+function validate() {
+    //Select element
+    const firstname = document.getElementById('firstname')
+    //Call the function which check the field
+    validateField(firstname, checkNameValue, 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.')
+
+    const lastname = document.getElementById('lastname')
+    validateField(lastname, checkNameValue, 'Veuillez entrer 2 caractères ou plus pour le champ du nom.')
+
+    const email = document.getElementById('email')
+    validateField(email, checkEmailValue, 'Veuillez entrer un email valide.')
+
+    const message = document.getElementById('message')
+    validateField(birthdate, checkMessageValue, 'Veuillez écrire un message de plus de 2 caractères.')
+
+    //create data object with each value of the form
+    const data = {
+        firstname: firstname.value,
+        lastname: lastname.value,
+        email: email.value,
+        message: message.value,
+    }
+    console.log(data)
+}
