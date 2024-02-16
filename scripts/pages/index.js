ea913eef38c8ff9021dc9c05f55d2e3d4b19cbdf
Vanessa-Peixoto <vanessa.peixoto.dev@gmail.com>
2024-02-16 14:45:28 +0100

    deploy

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -2,7 +2,7 @@
  * @description Recover the data.json
  */
 async function getPhotographers() {
-    const response = await fetch("/data/photographers.json");
+    const response = await fetch("./../../data/photographers.json");
     const photographers = await response.json();
     return photographers;
 }
